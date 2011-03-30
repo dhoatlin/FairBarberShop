963fc700dc17985166861ab1591aecf5a4b5a3fd
Dave Hoatlin <dhoatlin@gmail.com>
2011-03-30 02:13:13 -0400

    yeah, i was forking new processes instead of making threads... Threads are now working and customers wake up at the correct time

--- main.py
+++ main.py
@@ -5,55 +5,123 @@
 # Author: Dave Hoatlin
 
 import sys, os, signal, threading
+from threading import Thread
 
-class Customer:
+#semaphores we will need
+semaphores = {}
+
+#list of customer pids
+customers = []
+
+#lists containing customers. using pop/append for first come first server
+waitRoomCusts = []
+chairCusts = []
+
+class Customer(Thread):
 	arrival = 0
 	cutTime = 0
 	id = 0
-	def __init__(self, arrival, cutTime, id):
+	wakeupSem = 0
+	timeKeeper = 0
+	def __init__(self, arrival, cutTime, id, timeKeeper):
+		Thread.__init__(self)
 		self.arrival = arrival
 		self.cutTime = cutTime
 		self.id = id
+		self.wakeupSem = timeKeeper.wakeup(arrival)
+		
+		#tell the timeKeeper when to wake this customer
+		
 		
 	def run(self):
-		print 'running customer:', self.id, 'with arrival time:', self.arrival, 'and cut time:', self.cutTime
-		#enter barbershop (semaphore for waitroom) 
+		#global semaphores
+		#print 'running customer:', self.id, 'with arrival time:', self.arrival, 'and cut time:', self.cutTime
+		
+		#set wakeup time and wait until then
+		self.wakeupSem.acquire()
+		print 'customer:', self.id, 'woken up'
+		
+		'''
+		#enter barbershop (semaphore for waitroom)
+		semaphores['waitingRoom'].acquire()
+		waitRoomCusts.append(id)
+		print 'customer:', id, 'in waiting room'
+		
+		#wait for a chair to be available
+		semaphores['chair'].acquire()
+		sempahores['waitingRoom'].release()
+		waitRoomCusts.pop(0)
+		chairs.append(id)
+		print 'customer:', id, 'in waiting chair'
+		semaphores['chair'].release()
+		'''
 		#enter chair (semaphore for chairs) signal waitroom semaphore
 		#cut hair (semaphore for barbers) signal chair semaphore
 		#pay (semaphore for cashier) signal barber semaphore
 		#leave barbershop signal cashier semaphore
-		print 'customer:', self.id, 'has left'
 		
-class TimeKeeper:
+		#print 'customer:', self.id, 'has left'
+
+
+class TimeKeeper(Thread):
 	time = 0
-	
-	def handle(self, signum, _):
-		print signum
-		if(signum == 14): #14 is int value of SIGALRM
-			self.time += 1
-			print self.time
+	waitRequests = []
+
 	def __init__(self):
+		Thread.__init__(self)
 		signal.signal(signal.SIGALRM, self.handle)
 		signal.setitimer(signal.ITIMER_REAL, 1, 1)
 		
+	def handle(self, signum, _):
+		if(signum == 14): #14 is int value of SIGALRM
+			self.time += 1
+			#print self.time
+			if(self.waitRequests):
+				
+				if(int(self.waitRequests[0][0]) == int(self.time)):
+					print self.waitRequests[0][0]
+					self.waitRequests[0][1].release() #signal customer
+					self.waitRequests.pop(0) #remove customer from list
+
 	def run(self):
 		while(True):
 			a = True
+			
+	#add a process to the wakeup list and return the unique semaphore
+	def wakeup(self, time):
+		waitSem = threading.BoundedSemaphore(1)
+		
+		'''-----------------------
+		An awful hack to get the semaphore working...
+		
+		basically decrementing the semaphore's counter so when acquire is called
+		again the thread will be blocked
+		-----------------------'''
+		waitSem.acquire()
+		
+		self.waitRequests.append((time, waitSem))
+		self.waitRequests.sort() #sorting by wakeup time
+		return waitSem
 	
-class Barber:
+class Barber(Thread):
 	def __init__(self):
-		print 'barber created'
+		Thread.__init__(self)
+		
 		
 	def run(self):
 		time = 0
+		print 'barber started'
 		#wait for chair to be occupied(semaphore for chairs)
 		
 	
-class Cashier:
+class Cashier(Thread):
+	def __init__(self):
+		Thread.__init__(self)
+	
 	def run(self):
 		time = 0
 
-def spawnCustomers(fileName):
+def spawnCustomers(fileName, timeKeeper):
 	f = open(fileName)
 	checkFirst = False
 	count = 0
@@ -64,33 +132,21 @@ def spawnCustomers(fileName):
 			print 'Total customers: ', values[0][:-1] #[:-1] to ignore newline character
 			checkFirst = True
 		else:
-			pid = os.fork()
-			if(pid == 0):
-				cust = Customer(values[0], values[1][:-1], count)
-				cust.run()
-				exit()
+			cust = Customer(values[0], values[1][:-1], count, timeKeeper)
+			cust.start()
 			count += 1
-		#here i want to start a thread using values
-		#startCust(values[0], values[1])
 
 def spawnBarbers(totalBarbers):
 	count = 0
 	while (count < totalBarbers):
-		pid = os.fork()
-		if(pid == 0):
-			barber = Barber()
-			barber.run()
-			exit()
+		barber = Barber()
+		barber.start()
 		count += 1
 
 def startTimer():
-	pid = os.fork()
-	if(pid == 0):
-		timer = TimeKeeper()
-		timer.run()
-		exit()
-	else:
-		print 'timer created'
+	timer = TimeKeeper()
+	timer.start()
+	return timer
 	
 def handleCommands(args):
 	i = 1 #first arg is file name
@@ -101,18 +157,14 @@ def handleCommands(args):
 	while(i < len(args)):
 		if(args[i] == '-b'):
 			barbers = int(args[i+1])
-			print 'barbers set'
 			i += 2
 		elif(args[i] == '-c'):
 			chairs = int(args[i+1])
-			print 'chairs set'
 			i += 2
 		elif(args[i] == '-w'):
-			print 'waiting room set'
 			waitingRoom = int(args[i+1])
 			i += 2
 		elif(args[i] == '-i'):
-			print 'input file recieved'
 			inputFile = args[i+1]
 			i += 2
 		else:
@@ -137,18 +189,22 @@ def main():
 	#creating semaphores
 	semaphores = createSemaphores(commands['barbers'], commands['chairs'], commands['waitingRoom'])
 	
-	print 'making', commands['barbers'], 'barbers'
-	print 'making', commands['chairs'], 'chairs'
-	print 'making waiting room with size', commands['waitingRoom']
+	#print 'making', commands['barbers'], 'barbers'
+	#print 'making', commands['chairs'], 'chairs'
+	#print 'making waiting room with size', commands['waitingRoom']
 	
 	spawnBarbers(int(commands['barbers']))
-	#spawnCustomers('fairIn.txt')
-	#startTimer()
+	timer = startTimer()
+	spawnCustomers('fairQuick.txt', timer)
 	while True:
 		a = True
 	
 	
 if __name__ == '__main__':
 	main()
+	
+		
+
+
 	
 
