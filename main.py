0d7b622cb179525fdd8cb3c06661ce2828994f88
Dave Hoatlin <dhoatlin@gmail.com>
2011-03-31 11:46:10 -0400

    added comments and a few minor changes

--- main.py
+++ main.py
@@ -18,13 +18,20 @@ payCusts = []
 textColors = {'blue':'\033[1;34m', 'green':'\033[1;32m', 'yellow':'\033[1;33m',
 			  'reset':'\033[0m'}
 
-
+'''
+Customer is an object that runs in its own thread. It will wait until its
+designated arrival time. from there it will wait for a barber to be ready and
+then leave after paying the cashier
+'''
 class Customer(Thread):
+	#some object specific globals
 	arrival = 0
 	cutTime = 0
 	id = 0
 	wakeupSem = 0
 	timeKeeper = 0
+	
+	#initialize the customer object
 	def __init__(self, arrival, cutTime, id, timeKeeper):
 		Thread.__init__(self)
 		self.arrival = arrival
@@ -35,6 +42,8 @@ class Customer(Thread):
 		
 	'''
 	| customer procedure as defined by Hilzer
+	|
+	| This will begin when the thread is started
 	|
 	| var custNum
 	| begin
@@ -110,22 +119,34 @@ class Customer(Thread):
 		semaphores['queue2'].release()
 		semaphores['paid'][self.id].acquire()
 		
+		#leave the building and end the thread
 		string = str(self.timeKeeper.time) + ': customer: ' + str(self.id) + ' has left the building'
 		syncPrint('green', string)
 		
 
-
+'''
+Timekeeper is an object that runs in its own thread. It sets up an interval
+timer that sends a signal every second. TimeKeeper stores its own time value
+and increments it whenever SIGALRM is handled. It also tracks all the wakeup
+requests from the other threads and signals them when the delay time has
+expired
+'''
 class TimeKeeper(Thread):
-	
+	#object specific globals
 	time = 0
 	waitRequests = []
 	waitRequestsSem = threading.Semaphore(1)
 
+	#initialize TimeKeeper
 	def __init__(self):
 		Thread.__init__(self)
 		signal.signal(signal.SIGALRM, self.handle)
 		signal.setitimer(signal.ITIMER_REAL, 1, 1)
 		
+	'''
+	handle the SIGALRM signal and increment the internal time.
+	if any pending waitrequests have expired, signal that semaphore
+	'''
 	def handle(self, signum, _):
 		if(signum == signal.SIGALRM):
 			self.time += 1
@@ -139,6 +160,7 @@ class TimeKeeper(Thread):
 						self.waitRequests.remove(request) #remove request
 				self.waitRequestsSem.release()
 
+	#when started, run forever
 	def run(self):
 		while(True):
 			continue #run forever
@@ -156,6 +178,11 @@ class TimeKeeper(Thread):
 		self.waitRequestsSem.release()
 		return waitSem
 '''
+| Barber is an object that runs in its own thread. It waits for a customer to be
+| ready for a haircut and then cuts it for the specified duration.
+|
+| barber procedure as defined by Hilzer
+|
 | wait(cust_ready)
 | wait(mutex2)
 | dequeue(b_cust)
@@ -168,15 +195,22 @@ class TimeKeeper(Thread):
 | signal(barber_chair)
 '''
 class Barber(Thread):
+	#object specific globals
 	timeKeeper = 0
 	id = 0
+	
+	#initialize the barber
 	def __init__(self, timeKeeper, id):
 		Thread.__init__(self)
 		self.timeKeeper = timeKeeper
 		self.id = id
 		
+	#run the hilzer procedure when the thread starts
 	def run(self):
+		#file specific globals
 		global semaphores, barberCusts
+		
+		#handle new customers forever
 		while True:
 			#wait for a customer to be ready
 			semaphores['ready'].acquire()
@@ -196,9 +230,13 @@ class Barber(Thread):
 			semaphores['finish'][cust.id].release()
 			semaphores['leftBarber'][cust.id].acquire()
 			semaphores['barber'].release()
-			
-		
+
 '''
+| Cashier is an object that runs in its own thread. It waits for a customer to
+| be ready to pay and handles them in 0 seconds
+|
+| cashier procedure as defined by Hilzer
+|
 | wait(payment)
 | wait(mutex3)
 | dequeue2(c_cust)
@@ -209,16 +247,24 @@ class Barber(Thread):
 | signal(receipt[c_cust])
 '''
 class Cashier(Thread):
+	#object specific globals
 	timeKeeper = 0
 	id = 0
+	
+	#initialize cashier
 	def __init__(self, timeKeeper, id):
 		Thread.__init__(self)
 		self.timeKeeper = timeKeeper
 		self.id = id
 	
+	#run the procedure when the thread starts
 	def run(self):
+		#file specific globals
 		global semaphores, payCusts
+		
+		#handle customers forever
 		while True:
+			
 			#wait for a customer
 			semaphores['cashier'].acquire()
 			semaphores['queue2'].acquire()
@@ -229,54 +275,76 @@ class Cashier(Thread):
 			string = str(self.timeKeeper.time) + ': cashier received payment from customer ' + str(cust.id)
 			syncPrint('blue', string)
 			semaphores['paid'][cust.id].release()
-		
-		
 
+#create all the customer threads
 def spawnCustomers(custData, timeKeeper):
 	count = 0
 	customers = []
 	for customer in custData:
 		cust = Customer(customer[0], customer[1], count, timeKeeper)
 		customers.append(cust)
-		cust.setDaemon(True)
+		
+		#start the thread
 		cust.start()
 		count += 1
 	return customers
 	
+#create all the barber threads
 def spawnBarbers(totalBarbers, timeKeeper):
 	count = 0
 	barbers = []
 	while (count < totalBarbers):
 		barber = Barber(timeKeeper, count)
 		barbers.append(barber)
+		
+		#daemon value set to true, will terminate when nonDaemon threads are all
+		#complete
 		barber.setDaemon(True)
+		
+		#start the thread
 		barber.start()
 		count += 1
 	return barbers
 
+#create all the cashier threads
 def spawnCashiers(totalCashiers, timeKeeper):
 	count = 0
 	cashiers = []
 	while(count < totalCashiers):
 		cashier = Cashier(timeKeeper, count)
 		cashiers.append(cashier)
+		
+		#daemon value set to true, will terminate when nonDaemon threads are all
+		#complete
 		cashier.setDaemon(True)
+		
+		#start the thread
 		cashier.start()
 		count += 1
 	return cashiers
 
+#create the timer thread
 def startTimer():
 	timer = TimeKeeper()
+	
+	#daemon value set to true, will terminate when nonDaemon threads are all
+	#complete
 	timer.setDaemon(True)
+	
+	#start the thread
 	timer.start()
 	return timer
-	
+
+#interperate the commands
 def handleCommands(args):
 	i = 1 #first arg is file name
 	barbers = False
 	chairs = False
 	waitingRoom = False
 	inputFile = False
+	
+	#check every arg passed for the args we want to use
+	#exit if command not recognized
 	while(i < len(args)):
 		if(args[i] == '-b'):
 			barbers = int(args[i+1])
@@ -293,11 +361,16 @@ def handleCommands(args):
 		else:
 			print 'command not recognized'
 			sys.exit()
+	
+	#check that all commands were received, if not exit
 	if(barbers == False or chairs == False or waitingRoom == False or inputFile == False):
 		print 'must have -b -c -w and -i options set'
 		sys.exit()
+		
+	#return dictionary of commands
 	return {'barbers':barbers, 'chairs':chairs, 'waitingRoom':waitingRoom, 'inputFile':inputFile}	
 
+#create all the semaphores we will need
 def createSemaphores(barbers, chairs, waitingRoom, totalCustomers):
 	barberSem = threading.BoundedSemaphore(barbers)
 	chairSem = threading.BoundedSemaphore(chairs)
@@ -312,63 +385,81 @@ def createSemaphores(barbers, chairs, waitingRoom, totalCustomers):
 	leftBarberSem = []
 	paidSem = []
 	count = 0
+	
+	#unique semaphores for each customer
 	while count < int(totalCustomers):
 		finishSems.append(threading.Semaphore(0))
 		leftBarberSem.append(threading.Semaphore(0))
 		paidSem.append(threading.Semaphore(0))
 		count += 1
+		
+	#return a dictionary of all the semaphores
 	return {'barber':barberSem, 'chair':chairSem, 'waitingRoom':waitingRoomSem,
 			'ready':readySem, 'finish':finishSems, 'print':printSem,
 			'queue1':queueSem1, 'queue2':queueSem2, 'leftBarber':leftBarberSem,
 			'cashier':cashierSem, 'paid':paidSem, 'remain':custRemainSem}
 
+#parse the input file to grab each customer
 def parseInput(filename):
 	f = open(filename)
 	checkFirst = False
 	customerData = []
 	totalCustomers = 0
+	#for every line in the file
 	for line in f:
+		#split all the values on this line
 		values = line.split(' ')
+		
+		#if we are on the first line
 		if not checkFirst:
 			totalCustomers = values[0][:-1] #[:-1] to ignore newline character
 			print 'Total customers: ', totalCustomers 
 			checkFirst = True
+		
+		#otherwise grab the customer arrival and cut duration
 		else:
 			customerData.append((values[0], values[1][:-1]))
 	return totalCustomers, customerData
 
+#syncronizes printing and also converts the string to color
 def syncPrint(color, text):
 	global textColors, semaphores
 	semaphores['print'].acquire()
 	print textColors[color] + text + textColors['reset']
 	semaphores['print'].release()
 
+#main method that runs when program is started
 def main():
+	#globals specific to the file
 	global semaphores, customersRemaining
+	
 	#interpreting command line args
 	args = sys.argv
 	commands = handleCommands(args)
 	
-	inputs = parseInput('fairQuick.txt')
-
-	customersRemaining = int(inputs[0])
+	#parse input file
+	inputs = parseInput(commands['inputFile'])
 	
 	#creating semaphores
 	semaphores = createSemaphores(commands['barbers'], commands['chairs'], commands['waitingRoom'], inputs[0])
 	
+	#start the threads
 	timer = startTimer()
 	barbers = barberThreads = spawnBarbers(int(commands['barbers']), timer)
 	customers = custThreads = spawnCustomers(inputs[1], timer)
 	cashiers = cashierThreads = spawnCashiers(1, timer)
 
-	liveThreads = False
+	#wait for customer threads to finish
 	while len(custThreads) > 0:
 		for thread in custThreads:
 			if not thread.isAlive():
 				thread.join()
 				custThreads.remove(thread) #remove from pool of threads
 	
+	#remaining threads will terminate on their own because of the daemon feature
+	
 
+#run main method if program started from command line
 if __name__ == '__main__':
 	main()
 	
